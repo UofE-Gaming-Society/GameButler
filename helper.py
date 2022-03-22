f51867b1714a24224a917a04cf1d8f26b3a201b2
Richard Holmes <richard.gordon.holmes@gmail.com>
2022-03-22 21:21:20 +0000

    Add log method

--- helper.py
+++ helper.py
@@ -10,15 +10,18 @@ from typing import Callable
 def isGameRole(role: Role) -> bool:
     return str(role.colour) == str(COLOUR)
 
+async def log(message: str) -> None:
+    print(message)
+
 async def executeRoleCommand(ctx: SlashContext, role: Role, f: Callable[[SlashContext, Role], None], successMessage: str, invalidRoleMessage: str):
     try:
         if not isGameRole(role):
             if invalidRoleMessage != None and len(invalidRoleMessage) > 0:
                 await ctx.send(invalidRoleMessage)
-            print("Attempted to add role", role.name, "but", role.colour, "did not match", COLOUR)
         else:
             await f(ctx, role)
             if successMessage != None and len(successMessage) > 0:
                 await ctx.send(successMessage)
     except:
-        await ctx.send("Role does not exist")
+        await ctx.send("An error occured")
+        await log(f"An error occured when {ctx.author.mention} attempted to do something with {role.mention}")
