dda75a0738729d855339d473ef68e5225a39ffed
Richard Holmes <richard.gordon.holmes@gmail.com>
2022-03-24 10:47:46 +0000

    Add slash command error handling

--- botrunner.py
+++ botrunner.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from discord_slash import SlashCommand
 
 import config
+from ErrorHandler import ErrorHandler
 from GameButler import GameButler
 from GameRoleManager import GameRoleManager
 from MiscCommands import MiscCommands
@@ -18,10 +19,15 @@ if __name__ == "__main__":
     bot.add_cog(GameRoleManager(bot))
     bot.add_cog(SpamFilter(bot))
     bot.add_cog(MiscCommands(bot))
+    bot.add_cog(ErrorHandler(bot))
 
     bot.git_update = False
     bot.restart = False
 
     bot.run(config.TOKEN)
 
+    # return code 0 means neither restart nor update
+    # return code 2 means restart without update
+    # return code 4 means update without restart
+    # return code 6 means update and restart
     sys.exit(bot.restart * 2 + bot.git_update * 4)
