07a98efc956a525e03bcea7d0bd6a72c58e78d85
Maya Copeland <maya.copeland@protonmail.com>
2022-03-26 10:53:26 +0000

    you know what they say

--- runner.sh
+++ runner.sh
@@ -8,11 +8,11 @@ git_update()
 start_bot() 
 {
     python3 botrunner.py # edit this if /bin/python == python3
-    if [$? -eq 2]
+    if [ $? -eq 2 ]
     then
         start_bot
     fi
-    if [$? -eq 6]
+    if [ $? -eq 6 ]
     then 
         git_update
         start_bot
