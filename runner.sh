ad6476a8a4ca5b5a9cd568ba9ecfe064e494ade3
Maya Copeland <maya.copeland@protonmail.com>
2022-03-26 10:46:29 +0000

    I hate bash scripting.

--- runner.sh
+++ runner.sh
@@ -9,11 +9,11 @@ git_update()
 start_bot() 
 {
     python3 botrunner.py # edit this if /bin/python == python3
-    if [($? -eq 2) == 0]
+    if [$? -eq 2]
     then
         start_bot()
     fi
-    if [($? -eq 6) == 0]
+    if [$? -eq 6]
     then 
         git_update()
         start_bot()
