3e9c721bc8611995c751d0f5955e4819c659c91d
Maya Copeland <maya.copeland@protonmail.com>
2022-03-26 11:26:27 +0000

    i hate bash 2

--- runner.sh
+++ runner.sh
@@ -9,12 +9,12 @@ start_bot()
 {
     python3 botrunner.py # edit this if /bin/python == python3
     exit_code=$?
-    echo exit_code
-    if [ exit_code -eq 2 ]
+    echo $exit_code
+    if [ $exit_code -eq 2 ]
     then
         start_bot
     fi
-    if [ exit_code -eq 6 ]
+    if [ $exit_code -eq 6 ]
     then 
         git_update
         start_bot
