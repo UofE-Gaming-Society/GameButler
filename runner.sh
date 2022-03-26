d7d4bb829d2b29d96c66dda686d401ff85887359
Maya Copeland <maya.copeland@protonmail.com>
2022-03-26 10:44:32 +0000

    fixing the runner script?

--- runner.sh
+++ runner.sh
@@ -1,5 +1,6 @@
 #!/bin/sh 
 
+
 git_update() 
 {
     git pull
@@ -8,12 +9,12 @@ git_update()
 start_bot() 
 {
     python3 botrunner.py # edit this if /bin/python == python3
-    if [$? = 2]
+    if [($? -eq 2) == 0]
     then
         start_bot()
     fi
-    if [$? = 6]
-    then
+    if [($? -eq 6) == 0]
+    then 
         git_update()
         start_bot()
     fi
